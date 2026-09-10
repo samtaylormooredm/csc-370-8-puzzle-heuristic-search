81d34fe6c3f0ee72f928673a470a5da251c02956
Samantha Taylormoore <staylormoore@gmail.com>
2026-09-10 14:19:41 -0400

    Functions for goal state and blank index created

--- board.py
+++ board.py
@@ -1,19 +1,14 @@
-import random
-
-
 class Board:
-    """
-    
-    """
-    # 0 represents the empty space
+    """Represent a single state of the 8-puzzle board."""
     GOAL = (0, 1, 2,
             3, 4, 5,
             6, 7, 8)
 
     def __init__(self, tiles):
         """
-        Tiles should contain the numbers 0-8 exactly once.
-        0 represents the blank.
+        Initialize an 8-puzzle board with error handling. 
+        Tiles should contain the numbers 0-8 exactly once, such
+        that 0 represents the blank tile.
         """
         if len(tiles) != 9:
             raise ValueError("Board must contain exactly 9 positions.")
@@ -21,5 +16,49 @@ class Board:
         if set(tiles) != set(range(9)):
             raise ValueError("Board must contain each value 0-8 exactly once.")
 
-        # Store as tuple so Board states are immutable/hashable
+        # Store the board as an immutable tuple.
         self.tiles = tuple(tiles)
+
+    def __str__(self):
+        rows = []
+
+        for i in range(0, 9, 3):
+            row = self.tiles[i:i + 3]
+            row_string = " ".join("_" if value == 0 else str(value) for value in row)
+            rows.append(row_string)
+
+        return "\n".join(rows)
+    
+    def is_goal(self):
+        return self.tiles == Board.GOAL
+
+    def blank_index(self):
+        return self.tiles.index(0)
+   
+    def get_neighbors(self):
+        """
+        Generate all board states reachable in one legal move.
+
+        A legal move swaps the blank space with a tile directly above,
+        below, left, or right of it.
+
+        Returns
+        -------
+        list of Board
+            All neighboring board states reachable from the current state.
+        """
+        
+        # Represent as psuedocode for now
+            # locate blank
+            # determine blank row and column
+
+            # for each direction (up, down, left, right):
+            #     calculate destination position
+
+            #     if destination is valid:
+            #         copy board
+            #         swap blank with destination tile
+            #         add resulting board to neighbors
+
+            # return neighbors
+    
